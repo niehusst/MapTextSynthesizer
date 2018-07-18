580978a1b30bfe997791e550c5446272aca605dc
arthurhero <chenziwe@grinnell.edu>
2018-07-18 08:24:38 -0500

    added a bash script for easy install

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ PATH_TO_VENV=/home/chenziwe/virtualenv/test4
 #start to configure cmake
 
 mkdir -p ${PATH_TO_MTS}/${BUILD_FOLDER}
-pushd ${PATH_TO_MTS}/opencv/${BUILD_FOLDER}
+pushd ${PATH_TO_MTS}/${BUILD_FOLDER}
 
 cmake -D CMAKE_INSTALL_PREFIX=${PATH_TO_VENV}/local \
     ${PATH_TO_MTS}
