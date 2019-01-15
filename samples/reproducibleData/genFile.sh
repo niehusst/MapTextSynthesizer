83d6c8bb750b96bca2b2dfc2609762b69ebcf317
niehusst <liamniehusstaab@gmail.com>
2019-01-14 23:38:21 -0500

    conglomerated script contents

--- samples/reproducibleData/genFile.sh
+++ samples/reproducibleData/genFile.sh
@@ -3,6 +3,8 @@
 echo "making dir" $1
 
 mkdir $1
-touch $1/config$1.txt
-touch $1/font$1.txt
-touch $1/caption$1.txt
+
+#copy data form sample file into saved reproducible files
+cp ../caption.txt $1/caption$1.txt
+cp ../config.txt $1/config$1.txt
+cp ../fonts/custom.txt $1/font$1.txt
--- samples/reproducibleData/putData.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#script to copy data form sample file into saved reproducible files
-
-cp ../caption.txt $1/caption$1.txt
-cp ../config.txt $1/config$1.txt
-cp ../fonts/custom.txt $1/font$1.txt
